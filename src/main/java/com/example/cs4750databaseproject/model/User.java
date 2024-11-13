652862a4dd5131e23e4c67f2694f0574f58f4be7
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-11-13 15:00:31 -0500

    added user getter and setters in model

--- src/main/java/com/example/cs4750databaseproject/model/User.java
+++ src/main/java/com/example/cs4750databaseproject/model/User.java
@@ -10,17 +10,35 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private long patientID;
-//
-//    @Column(name = "email")
-//    private String email;
-//
-//    @Column(name = "password")
-//    private String password;
-//
-//    @Column(name="date_joined")
-//    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="dd-MM-yyyy", timezone = "UTC")
-//    @Temporal(TemporalType.DATE)
-//    private Date date_joined;
+    private long user_id;
 
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "watchlist_id")
+    private long watchlist_id;
+
+    @Column(name="date_joined")
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="dd-MM-yyyy", timezone = "UTC")
+    @Temporal(TemporalType.DATE)
+    private Date date_joined;
+
+    public long getUserID(){
+        return this.user_id;
+    }
+    public String getUserEmail(){
+        return this.email;
+    }
+    public long getUserWatchlistId(){
+        return this.watchlist_id;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
 }
