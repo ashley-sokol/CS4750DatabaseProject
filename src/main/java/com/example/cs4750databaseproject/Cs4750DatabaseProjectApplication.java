9c596a87916ae161bdd4f8204f35b935bbeb01c3
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-11-13 12:04:39 -0500

    Initial Project Setup

---
+++ src/main/java/com/example/cs4750databaseproject/Cs4750DatabaseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs4750databaseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4750DatabaseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs4750DatabaseProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cs4750databaseproject/Cs4750DatabaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs4750databaseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4750DatabaseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
