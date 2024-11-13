9400b69215842b4d2696d124e130930e43add401
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-11-13 12:38:40 -0500

    added some column data for user model file

--- src/main/java/com/example/cs4750databaseproject/model/User.java
+++ src/main/java/com/example/cs4750databaseproject/model/User.java
@@ -1,10 +1,26 @@
 package com.example.cs4750databaseproject.model;
 
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 
+import java.util.Date;
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long patientID;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
-    private Date dateJoined;
+
+    @Column(name="date_joined")
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="dd-MM-yyyy", timezone = "UTC")
+    @Temporal(TemporalType.DATE)
+    private Date date_joined;
 
 }
