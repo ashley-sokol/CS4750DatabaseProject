2d62c0bd167c43d5ac08abe184366c293ee61126
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-11-13 12:31:48 -0500

    created initial user controller file

---
+++ src/main/java/com/example/cs4750databaseproject/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.cs4750databaseproject.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api")
+public class UserController {
+//    @Autowired
+//    UserRepository userrepository;
+//
+//    @GetMapping("/user")
+}
