671b59870f7ef555fd12b1d520fa3c0529ed875b
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-11-13 15:00:52 -0500

    added post method for creating a new user

--- src/main/java/com/example/cs4750databaseproject/controller/UserController.java
+++ src/main/java/com/example/cs4750databaseproject/controller/UserController.java
@@ -1,13 +1,28 @@
 package com.example.cs4750databaseproject.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.example.cs4750databaseproject.repository.*;
+import com.example.cs4750databaseproject.model.*;
+//User API should include creating, deleting, or updating a user's email or password,
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class UserController {
-//    @Autowired
-//    UserRepository userrepository;
-//
-//    @GetMapping("/user")
+    UserRepository userrepository;
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createPatient(@RequestBody User user) {
+           if (userrepository.findUserByEmail(user.getUserEmail()) != null)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+
+        // Save the user to the database
+        User createdUser = userrepository.save(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+
 }
