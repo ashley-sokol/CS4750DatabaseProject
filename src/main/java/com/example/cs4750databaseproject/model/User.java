d880e0289f23278820c22a543c5c96fe0b19778c
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-11-13 12:35:56 -0500

    created initial user model file

---
+++ src/main/java/com/example/cs4750databaseproject/model/User.java
@@ -0,0 +1,10 @@
+package com.example.cs4750databaseproject.model;
+
+import java.util.Date;
+
+public class User {
+    private String email;
+    private String password;
+    private Date dateJoined;
+
+}
